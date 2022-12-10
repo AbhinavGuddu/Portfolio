4886cb084ff561213ed3b95d38f6bf1b4cd7a770
AbhinavGuddu <abhinavguddu99@gmail.com>
2022-12-10 12:11:36 +0530

    first message

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Footer() {
+    return (
+      <div class="text-center">
+        This portfolio is made in React js <span>By <strong> Abhinav Guddu</strong></span>
+    </div>
+    );
+  }
+  export default Footer;
