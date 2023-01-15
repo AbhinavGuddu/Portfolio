41d4381b38a8229634d109d5d34213ba5ac5f992
Abhinav Guddu <66636825+AbhinavGuddu@users.noreply.github.com>
2023-01-15 18:35:13 +0530

    Update ReadingCorner.js

--- src/components/ReadingCorner.js
+++ src/components/ReadingCorner.js
@@ -11,14 +11,13 @@ const ReadingCorner = () => {
         <p className="lg:w-2/3 mx-auto leading-relaxed text-base mb-8">
         I am frequently inspired by the amazing things people do, and I collect these sources of inspiration in a dedicated space where I can reflect on and draw from these ideas for personal projects and pursuits.
         </p>
-
         <div className="flex flex-wrap m-2">
-          {blogs.map((testimonial) => (
-            <a
-              href={testimonial.url}
-              target="blank"
-              className="p-1 md:w-1/2 w-3/4 "
-            >
+        {blogs.map((testimonial) => (
+          <a
+            href={testimonial.url}
+            target="blank"
+            className="p-1 md:w-1/2 w-full "
+          >
               <div className="h-full bg-gray-800 bg-opacity-40 p-4 rounded hover:bg-gray-700 rounded text-lg">
                 <CodeBracketSquareIcon className="inline w-8 text-gray-500 mb-1" />
                 <span className="title-font font-small text-white ml-4">
